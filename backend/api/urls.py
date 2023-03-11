f87fff924748630a4b0020ebe818723e17d07b97
Alexandr Sharapov <sashasharapov1999@yandex.ru>
2023-03-11 14:47:14 +0300

    deploy

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import IngredientsViewSet, RecipeViewSet, TagsViewSet
 
 router = DefaultRouter()
-router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientsViewSet, basename='ingredients')
 router.register('tags', TagsViewSet, basename='tags')
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
